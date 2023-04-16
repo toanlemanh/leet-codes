02716fc47dd603820002cbc76bc0862b64ecaea0
Toan Tom <chen22112k3@gmail.com>
2023-04-16 22:24:20 +0700

    push sortingsquare977

---
+++ SoritngSquare977.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SoritngSquare977 {
+    public int[] sortedSquares(int[] nums) {
+        int [] square = new int [nums.length];
+        // squaring all
+        for ( int i = 0; i < nums.length; i++ ){
+            square[i] = (int) Math.pow( nums[i],2);
+        }
+
+        // then sorting them again
+        Arrays.sort(square);
+     return square;
+    }
+}
