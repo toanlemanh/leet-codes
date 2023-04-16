a9ec37819f4fa704651ac78abad8ab5ee4da6e17
Toan Tom <chen22112k3@gmail.com>
2023-04-16 21:34:41 +0700

    update searchInsert035 with O(logn)

--- SearchInsert035.java
+++ SearchInsert035.java
@@ -1,20 +1,46 @@
 package leetcode;
 
 public class SearchInsert035 {
-    class Solution {
-        public int searchInsert(int[] nums, int target) {
-            for ( int i = nums.length-1; i >= 0; i--){
-                if (nums [i] == target)
-                    return i;
-            }
-            // if not
-            // compare
-            for ( int i = nums.length-1; i >= 0; i--){
-                if ( nums[i] < target)
-                    return i+1;
-            }
-            return 0;
 
-        }
+
+    // O(log(n))
+    //Given a sorted array of distinct integers and a target value, return the index if the target is found.
+    // If not, return the index where it would be if it were inserted in order.
+    // You must write an algorithm with O(log n) runtime complexity.
+//        public static int searchInsert(int[] nums, int target) {
+//                for ( int i = nums.length-1; i >= 0; i--){
+//                    if (nums [i] == target)
+//                        return i;
+//                }
+//                // if not
+//                // compare
+//                for ( int i = nums.length-1; i >= 0; i--){
+//                    if ( nums[i] < target)
+//                        return i+1;
+//                }
+//                return 0;
+//
+//            }
+
+    public static int searchInsert(int[] nums, int target) {
+         int left = 0; int right = nums.length - 1;
+         while ( left <= right ){
+             int mid = (left + right) / 2;  // để tránh
+             if ( nums[mid] == target )
+                 return mid;
+             else if ( nums[mid] < target )
+                 left = mid + 1;
+             else
+                 right = mid - 1;
+         }
+         return left;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {0,1,2,4,7,8,9};
+        int x = searchInsert(arr,10);
+        System.out.println(x);
     }
 }
+
+
